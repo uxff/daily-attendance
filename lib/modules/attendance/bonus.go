eaa59ea0b6f23c0fa46349d8654b3f47e6157b3e
uxff <uxff@qq.com>
2018-12-03 08:49:45 +0800

    update attendance share

--- lib/modules/attendance/bonus.go
+++ lib/modules/attendance/bonus.go
@@ -27,4 +27,35 @@ func ListUserWastageLog(Uid int) []*models.WastageShare {
 	return list
 }
 
+// share all missed
+func ShareMissedAttendance(jal *models.JoinActivityLog) {
+	// list all activity
+	activities := ListActivities()
+	for _, act := range activities {
+		missedJals := ListMissedJal(act.Aid)
+		successJals := ListSuccessJal(act.Aid)
+		for _, mjal := range missedJals {
+			ShareMissedJal(mjal, successJals)
+		}
+	}
+}
 
+//
+func ListMissedJal(Aid int) []*models.JoinActivityLog {
+	return nil
+}
+
+func ShareMissedJal(missedJal *models.JoinActivityLog, successJals[]*models.JoinActivityLog) error {
+	//goldsWillShare := missedJal.JoinUtlId.Price
+	//allSuccessFeederGoods := GetAllSuccessorGolds()
+	//
+	// for _, sjal := range successJals {
+	//    goods := goldsWillShare * (sjal.JoinUtlId.Price*sjal.Step/allSuccessFeederGoods)
+	//    DispatchBonus(sjal.Aid, sjal.Uid, goods)
+	// }
+	return nil
+}
+
+func ListSuccessJal(Aid int)[]*models.JoinActivityLog {
+	return nil
+}
