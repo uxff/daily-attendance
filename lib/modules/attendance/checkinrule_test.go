fcb4e7c36186a4deb31b52334702117e54a96923
uxff <uxff@qq.com>
2018-12-12 23:13:48 +0800

    update checkIn rule

--- lib/modules/attendance/checkinrule_test.go
+++ lib/modules/attendance/checkinrule_test.go
@@ -7,17 +7,18 @@ import (
 
 func TestCheckInRule(t *testing.T) {
 	jalId := 1
-	key := "aaa"
+	key := "a"
 	now := time.Now()
+	t.Logf("t=%v weekday=%d yearday=%d day=%d", now, now.Weekday(), now.YearDay(), now.Day())
 
 	cirm := Json2CheckInRule(`{"s":{"dayspan":""},"i":{"dayspan":"00-10"},"H":{"timespan":"10:00-29:59"},"D":{"timespan":"18:00-23:59"},"W":{"dayspan":"03-04"},"M":{"dayspan":"25-28"},"Y":{"dayspan":"05-12"}}`)
 	t.Errorf("cirm=%+v", cirm)
 
-	t.Logf("elem %v S=%+v", cirm["s"].IsSecondSpanValid(), cirm["s"].GetSecondlyCheckInScheduleElem(jalId, key, now))
-	t.Logf("elem %v i=%+v", cirm["i"].IsSecondSpanValid(), cirm["i"].GetMinutelyCheckInScheduleElem(jalId, key, now))
-	t.Logf("elem %v H=%+v", cirm["H"].IsMinuteSpanValid(), cirm["H"].GetHourlyCheckInScheduleElem(jalId, key, now))
-	t.Logf("elem %v D=%+v", cirm["D"].IsHourSpanValid(), cirm["D"].GetDailyCheckInScheduleElem(jalId, key, now))
-	t.Logf("elem %v W=%+v", cirm["W"].IsWeekdaySpanValid(), cirm["W"].GetWeeklyCheckInScheduleElem(jalId, key, now))
-	t.Logf("elem %v M=%+v", cirm["M"].IsDaySpanValid(), cirm["M"].GetMonthlyCheckInScheduleElem(jalId, key, now))
-	t.Logf("elem %v Y=%+v", cirm["Y"].IsDaySpanValid(), cirm["Y"].GetYearlyCheckInScheduleElem(jalId, key, now))
+	t.Logf("%v S=%+v", cirm["s"].IsSecondSpanValid(), cirm["s"].GetSecondlyCheckInScheduleElem(jalId, key, now))
+	t.Logf("%v i=%+v", cirm["i"].IsSecondSpanValid(), cirm["i"].GetMinutelyCheckInScheduleElem(jalId, key, now))
+	t.Logf("%v H=%+v", cirm["H"].IsMinuteSpanValid(), cirm["H"].GetHourlyCheckInScheduleElem(jalId, key, now))
+	t.Logf("%v D=%+v", cirm["D"].IsHourSpanValid(), cirm["D"].GetDailyCheckInScheduleElem(jalId, key, now))
+	t.Logf("%v W=%+v", cirm["W"].IsWeekdaySpanValid(), cirm["W"].GetWeeklyCheckInScheduleElem(jalId, key, now))
+	t.Logf("%v M=%+v", cirm["M"].IsDaySpanValid(), cirm["M"].GetMonthlyCheckInScheduleElem(jalId, key, now))
+	t.Logf("%v Y=%+v", cirm["Y"].IsDaySpanValid(), cirm["Y"].GetYearlyCheckInScheduleElem(jalId, key, now))
 }
